5077e2c4dbf9e1bf37dd29607d43cd16e0680bdf
Mikhail Kamnev <Mikhail Kamnev@localhost>
2019-02-13 17:48:00 +0700

    shown flag moved into overlay

--- WpfApp1/ViewModels/MainViewModel.cs
+++ WpfApp1/ViewModels/MainViewModel.cs
@@ -1,32 +1,14 @@
 ﻿namespace WpfApp1.ViewModels
 {
-	using System.Windows.Input;
-	using Prism.Commands;
 	using Prism.Mvvm;
 
 	internal class MainViewModel : BindableBase
 	{
-		private bool _isOverlayShown;
-
 		public MainViewModel()
 		{
-			ToggleOverlay = new DelegateCommand(ExecuteToggleOverlay);
-			Overlay = new OverlayViewModel(() => IsOverlayShown = false, 20);
+			Overlay = new OverlayViewModel(20);
 		}
-
-		public bool IsOverlayShown
-		{
-			get => _isOverlayShown;
-			set => SetProperty(ref _isOverlayShown, value);
-		}
-
-		public ICommand ToggleOverlay { get; }
 
 		public OverlayViewModel Overlay { get; }
-
-		private void ExecuteToggleOverlay()
-		{
-			IsOverlayShown = !IsOverlayShown;
-		}
 	}
 }
--- WpfApp1/ViewModels/OverlayViewModel.cs
+++ WpfApp1/ViewModels/OverlayViewModel.cs
@@ -1,6 +1,5 @@
 ﻿namespace WpfApp1.ViewModels
 {
-	using System;
 	using System.Collections.ObjectModel;
 	using System.Windows.Input;
 	using Prism.Commands;
@@ -8,11 +7,12 @@
 
 	internal class OverlayViewModel : BindableBase
 	{
-		private readonly Action _hideCallback;
+		private bool _isShown;
 
 		public OverlayViewModel()
-			:this(0)
+			: this(0)
 		{
+			IsShown = true;
 		}
 
 		public OverlayViewModel(int n)
@@ -24,18 +24,22 @@
 			{
 				Items.Add(new OverlayItemViewModel($"Элемент №{i + 3}"));
 			}
-			Hide = new DelegateCommand(() => _hideCallback?.Invoke());
-		}
 
-		public OverlayViewModel(Action hideCallback, int n)
-			: this(n)
-		{
-			_hideCallback = hideCallback;
+			Hide = new DelegateCommand(() => IsShown = false);
+			Toggle = new DelegateCommand(() => IsShown = !IsShown);
 		}
 
 		public ICommand Hide { get; }
 
+		public bool IsShown
+		{
+			get => _isShown;
+			set => SetProperty(ref _isShown, value);
+		}
+
 		public ObservableCollection<OverlayItemViewModel> Items { get; } =
 			new ObservableCollection<OverlayItemViewModel>();
+
+		public ICommand Toggle { get; }
 	}
 }
